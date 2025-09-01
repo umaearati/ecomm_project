9ab853e73e070688260c3b07f82f50888806bace
MAHI <uuma8628@gmail.com>
2025-09-01 17:30:32 +0100

    created project2

---
+++ get_lib_versions.py
@@ -0,0 +1,27 @@
+import importlib.metadata
+packages = [
+    "langchain_core",
+    "langchain",
+    "python-dotenv",
+    # "ipykernel",
+    # "langchain_groq",
+    # "langchain_google_genai",
+    # "langchain-community",
+    # "faiss-cpu",
+    # "structlog",
+    # "PyMuPDF",
+    # "pylint",
+    # "langchain-core",
+    # "pytest",
+    # "streamlit",
+    # "fastapi",
+    # "uvicorn",
+    # "python-multipart",
+    # "docx2txt"
+]
+for pkg in packages:
+    try:
+        version = importlib.metadata.version(pkg)
+        print(f"{pkg}=={version}")
+    except importlib.metadata.PackageNotFoundError:
+        print(f"{pkg} (not installed)")
